bab43e6e5842d6179bee8fc180a8c422c09aac1c
Doniyor <ibrokhimovdoniyorbek@gmail.com>
2023-12-29 12:28:19 +0500

    Initial Commit

---
+++ Hotel Test App/Hotel_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hotel_Test_AppApp.swift
+//  Hotel Test App
+//
+//  Created by Doniyorbek Ibrokhimov on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Hotel_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
